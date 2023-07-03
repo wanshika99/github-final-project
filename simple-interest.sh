08a85c80bc9c781dc3f68177ff881bb9f52776c9
wanshika99 <75947798+wanshika99@users.noreply.github.com>
2023-07-03 09:23:01 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # wanshika99
    # Input:
    # p, principal amount
    # t, time period in years
